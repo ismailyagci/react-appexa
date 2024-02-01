7a53eb0b2afaf7661eeb4566a9857028b901e173
İsmail Yağcı <ismailyagci@Ismails-Mac-mini.local>
2024-02-02 00:19:02 +0300

    v1.0.4

--- src/validation/types/phoneNumber/index.js
+++ src/validation/types/phoneNumber/index.js
@@ -1,5 +1,6 @@
 const phoneNumber = (data) => {
-  const pattern = /^\+?([0-9]{1,3})?([ .-]?)(\(?[0-9]{1,4}\)?)([ .-]?)([0-9]{1,4})([ .-]?)([0-9]{1,4})$/;
+  const pattern = /^\+?(\d{1,3})?[-. ]?\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
   return data.match(pattern) !== null;
 };
+
 export default phoneNumber;
