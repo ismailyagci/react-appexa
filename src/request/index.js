e5c99231d05daf7968360a56bd6450427e982ecf
İsmail Yağcı <ismailyagci@Ismails-Mac-mini.local>
2024-02-04 01:10:31 +0300

    v1.0.5

--- src/request/index.js
+++ src/request/index.js
@@ -8,10 +8,15 @@ class RequestManager {
     this.config = {};
     this.request = {
       setHeader: this.setHeader,
-      getRequestConfig: this.getRequestConfig
+      getHeader: this.getHeader,
+      getRequestConfig: this.getRequestConfig,
     };
     this.createListeners();
     this.createInterceptors();
+  }
+
+  getHeader = () => {
+    return this.headers;
   };
 
   getRequestConfig = () => {
@@ -20,21 +25,21 @@ class RequestManager {
 
   createInterceptors = () => {
     this.axios.interceptors.request.use(
-      config => {
+      (config) => {
         config.baseURL = this.config.baseUrl;
         Object.entries(this.headers).forEach(([key, value]) => {
-          config.headers[key] = value
-        })
+          config.headers[key] = value;
+        });
         return config;
       },
-      error => {
+      (error) => {
         return Promise.reject(error);
       }
     );
   };
 
   setHeader = (key, value) => {
-    this.headers[key] = value
+    this.headers[key] = value;
   };
 
   isSuccessRequest(response) {
@@ -43,35 +48,48 @@ class RequestManager {
 
   getMethod = async (requestConfig, params) => {
     return await new Promise((resolve, reject) => {
-      this.axios.get(requestConfig.url, { params }, {
-        headers: requestConfig?.headers || {}
-      }).then((res) => {
-        if (this.isSuccessRequest(res)) return resolve(res.data);
-        else reject(res)
-      }).catch(reject);
-    })
+      this.axios
+        .get(
+          requestConfig.url,
+          { params },
+          {
+            headers: requestConfig?.headers || {},
+          }
+        )
+        .then((res) => {
+          if (this.isSuccessRequest(res)) return resolve(res.data);
+          else reject(res);
+        })
+        .catch(reject);
+    });
   };
 
   postMethod = async (requestConfig, params) => {
     return await new Promise((resolve, reject) => {
-      this.axios.post(requestConfig.url, params, {
-        headers: requestConfig?.headers || {}
-      }).then((res) => {
-        if (this.isSuccessRequest(res)) return resolve(res.data);
-        else reject(res)
-      }).catch(reject);
-    })
-  }
+      this.axios
+        .post(requestConfig.url, params, {
+          headers: requestConfig?.headers || {},
+        })
+        .then((res) => {
+          if (this.isSuccessRequest(res)) return resolve(res.data);
+          else reject(res);
+        })
+        .catch(reject);
+    });
+  };
 
   generateRequestMethods() {
-    const requestKeys = Object.keys(this?.config || {}).filter((key) => key !== "baseUrl");
+    const requestKeys = Object.keys(this?.config || {}).filter(
+      (key) => key !== "baseUrl"
+    );
     const methods = {
       get: this.getMethod,
       post: this.postMethod,
     };
     requestKeys.forEach((requestKey) => {
       const request = this.config[requestKey];
-      this.request[requestKey] = (params) => methods[request.type](request, params)
+      this.request[requestKey] = (params) =>
+        methods[request.type](request, params);
     });
     container.set("requestMethodsCreated", true);
   }
@@ -82,8 +100,8 @@ class RequestManager {
         this.config = value.request;
         this.generateRequestMethods();
       }
-    })
-  }
+    });
+  };
 }
 
 export default new RequestManager().request;
